e59815df842bbe6c1bf94e7ee64c808a52a4657b
ali-aboulsauood <aliaboulsauood@gmail.com>
2025-02-16 07:00:17 +0200

    Modify function to return ERROR with negative numbers
    Tests passed:
    
    a - repeatString.spec.js (8 tests total; 6 passed, 2 skipped)
        1 - repeats the string
        2 - repeats the string many times
        3 - repeats the string one time
        4 - repeats the string zero times
        5 - does not use the built-in String repeat method
        6 - returns ERROR with negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
 const repeatString = function(string, numberOfTimes) {
     let resultString = "";
 
+    if (+numberOfTimes < 0) {
+        return 'ERROR';
+    }
+
     for (let i = 1; i <= numberOfTimes; i++) {
         resultString += string;
     }

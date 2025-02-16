247b7782adfa714f6a58df02c30126c9ad626940
ali-aboulsauood <aliaboulsauood@gmail.com>
2025-02-16 06:49:50 +0200

    Modify function to repeat the string
    Tests passed:
    
    a - repeatString.spec.js (8 tests total; 1 passed, 7 skipped)
        1 - repeats the string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfTimes) {
+    let resultString = "";
 
+    for (let i = 1; i <= numberOfTimes; i++) {
+        resultString += string;
+    }
+
+    return resultString;
 };
 
 // Do not edit below this line

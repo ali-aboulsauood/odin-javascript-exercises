1a285c7436008981e1217eb8ce7359dc03ba271a
ali-aboulsauood <aliaboulsauood@gmail.com>
2025-02-16 08:09:57 +0200

    Modify function to remove a single element
    Tests passed (8 total; 7 skipped, 1 passed)
        1 - removes a single element

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementToRemove) {
+    const indexOfElementToRemove = array.indexOf(elementToRemove);
+
+    return array.slice(0, indexOfElementToRemove).concat(array.slice(indexOfElementToRemove + 1));
 };
 
 // Do not edit below this line

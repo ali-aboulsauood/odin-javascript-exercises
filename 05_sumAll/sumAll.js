22119671e7e38a61f0b9b6d369213564db69ce18
ali-aboulsauood <aliaboulsauood@gmail.com>
2025-02-17 19:28:20 +0200

    Correct Solution
    - Use `Number.isInteger` instead of `Math.trunc` to check for non-integer values
    - Use array destructuring instead of `Math.min` to swap parameter values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,16 @@
 const sumAll = function(start, end) {
     const includesNaN = (Number.isNaN(start) || Number.isNaN(end));
-    const includesNonInteger = (start !== Math.trunc(start) || end !== Math.trunc(end));
+    // const includesNonInteger = (start !== Math.trunc(start) || end !== Math.trunc(end));
+    const includesNonInteger = !(Number.isInteger(start) && Number.isInteger(end));
     const includesNegative = (start < 0 || end < 0);
 
     if (includesNaN || includesNonInteger || includesNegative) return 'ERROR';
 
-    const start_ = Math.min(start, end), end_ = Math.max(start, end);
+    // const start_ = Math.min(start, end), end_ = Math.max(start, end);
+    if (start > end) [start, end] = [end, start];
 
     let sum = 0;
-    for (let i = start_; i <= end_; i++) sum += i;
+    for (let i = start; i <= end; i++) sum += i;
 
     return sum;
 };

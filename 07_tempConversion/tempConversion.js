3c9713c0c2ac5be6ac8dc35f5424e65e78d4c481
ali-aboulsauood <aliaboulsauood@gmail.com>
2025-02-17 20:01:34 +0200

    Passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  const tempInCelsius = (tempInFahrenheit - 32) * (5 / 9);
+
+  // The method `Number.toFixed` returns a String.
+  return +tempInCelsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  let tempInFahrenheit = tempInCelsius * (9 / 5) + 32;
+
+  // The method `Number.toFixed` returns a String.
+  return +tempInFahrenheit.toFixed(1);
 };
 
 // Do not edit below this line

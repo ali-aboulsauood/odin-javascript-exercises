b6e788aff31ed721fa5b66c220de81658cda8e46
ali-aboulsauood <aliaboulsauood@gmail.com>
2025-02-17 18:44:46 +0200

    Passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    const includesNaN = (Number.isNaN(start) || Number.isNaN(end));
+    const includesNonInteger = (start !== Math.trunc(start) || end !== Math.trunc(end));
+    const includesNegative = (start < 0 || end < 0);
 
+    if (includesNaN || includesNonInteger || includesNegative) return 'ERROR';
+
+    const start_ = Math.min(start, end), end_ = Math.max(start, end);
+
+    let sum = 0;
+    for (let i = start_; i <= end_; i++) sum += i;
+
+    return sum;
 };
 
 // Do not edit below this line
